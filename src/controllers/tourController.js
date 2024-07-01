17798aa84aec56fd54bac8b96654056a00d63f81
Ange Michel <angemicheluwizeye@gmail.com>
2024-07-01 11:47:25 +0200

    Making API better_Pagination

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -25,7 +25,16 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select("-__v");
     }
-    const tours = await query;
+    // PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    const tours = await query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
     //SEND RESPONSE
     res.status(200).json({
       status: "Suceess",
