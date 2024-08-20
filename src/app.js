e9697b23fcc629bc22ab1a2afdae79b08f1ff6b1
Angemichel12 <angemicheluwizeye@gmail.com>
2024-08-20 20:39:43 +0200

    error handling

--- src/app.js
+++ src/app.js
@@ -12,11 +12,19 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
-app.all('*', (req, res, next)=>{
-  res.status(404).json({status:"Not Found",
-    message:`Can't find ${req.originalUrl} on server`
-  })
-})
 app.use(express.static(`${__dirname}/public`));
+app.all('*', (req, res, next)=>{
+  const err = new Error(`Can't find ${req.originalUrl} on server`);
+  err.status ="Not Found",
+  err.statusCode=404
+ next(err); 
+})
+app.use((err,req,res,next)=>{
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "Error";
+  res.status(err.statusCode).json({status:err.status, message:err.message});
+})
+
+
 
 module.exports = app;
